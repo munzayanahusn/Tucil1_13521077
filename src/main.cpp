95d4bdd3f74bf287871caccf7fa5f09e03c628e7
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-01-21 12:07:57 +0700

    first commit add main

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// TUGAS KECIL 1 STRATEGI ALGORITMA
+// Nama : Husnia Munzayana
+// NIM  : 13521077
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Inisiasi Variabel
+int x;
+
+// Program Utama
+int main()
+{
+    cin >> x;
+    cout << "Hai \n"
+         << x;
+    return 0;
+}
